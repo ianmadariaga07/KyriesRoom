9a282d16f9492ce8d973f7271c0e54d17c20ab90
ianmadariaga07 <iantranquis11@gmail.com>
2026-07-19 00:08:06 -0600

    style: cambiamos de nombre nuestro componente transaction-modal

--- nexus-web/src/app/components/transaction-modal/transaction-modal.spec.ts
+++ nexus-web/src/app/components/transaction-modal/transaction-modal.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NewTransactionModal } from './new-transaction-modal';
+import { TransactionModal } from './transaction-modal';
 
-describe('NewTransactionModal', () => {
-  let component: NewTransactionModal;
-  let fixture: ComponentFixture<NewTransactionModal>;
+describe('TransactionModal', () => {
+  let component: TransactionModal;
+  let fixture: ComponentFixture<TransactionModal>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NewTransactionModal]
+      imports: [TransactionModal]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(NewTransactionModal);
+    fixture = TestBed.createComponent(TransactionModal);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
---
+++ nexus-web/src/app/components/transaction-modal/transaction-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-modal',
+  imports: [],
+  templateUrl: './transaction-modal.html',
+  styleUrl: './transaction-modal.css',
+})
+export class TransactionModal {
+
+}
