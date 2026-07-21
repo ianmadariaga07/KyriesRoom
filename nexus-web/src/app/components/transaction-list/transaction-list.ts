ac263753bfa3d7a3750da2230cae7a68404e7da4
ianmadariaga07 <iantranquis11@gmail.com>
2026-07-21 09:36:50 -0600

    refactor(transactions): modularizacion de la creacion de transacciones - Migracion de logica de FormGroup, validaciones y peticiones HTTP al nuevo componente - Implementacion de comunicacion Padre-Hijo mediante @Input y @Output para estado y lista de subcuentas - Limpieza y reduccion del tamaño del componente principal transaction-list

--- nexus-web/src/app/components/transaction-list/transaction-list.ts
+++ nexus-web/src/app/components/transaction-list/transaction-list.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, signal, computed, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { Transaction, TransactionType} from '../../interfaces/transaction.interface';
 import { TransactionService} from '../../services/transaction';
@@ -40,7 +40,6 @@ export class TransactionList implements OnInit {
   public idDelete: string = '';
 
   //quitamos el constructor y utilizamos inject que es la inyeccion de dependencias moderna
-  private fb = inject(FormBuilder);
   private messageService = inject(MessageService);
   private subAccountService = inject(SubAccountService);
   private transactionService = inject(TransactionService);
@@ -50,7 +49,6 @@ export class TransactionList implements OnInit {
   ngOnInit() {
     this.loadTransactions();
     this.loadSubAccounts();
-    this.setupFormLogic();
     this.primeng.ripple.set(true);
   }
 
@@ -77,93 +75,10 @@ export class TransactionList implements OnInit {
     return total;
   });
 
- //Total App sumando dos anteriores
+  //Total App sumando dos anteriores
   public globalTotalApp = computed(() => {
     return this.globalRealBalance() + this.globalCreditDebt();
   });
-
-  public transactionForm: FormGroup = this.fb.group({
-    amount: [null, [Validators.required, Validators.min(0.01)]],
-    type: ['expense', Validators.required], //valores: income, expense, payment
-    subAccountId: ['', Validators.required],
-    method: ['debito', Validators.required], //valores: debito, credito
-    concept: ['', Validators.required],
-    date: [new Date(), Validators.required],
-    description: ['']
-  });
-
-  public onSubmit() {
-    if (this.transactionForm.invalid) {
-      //finge que el usuario ya dio clic y ensucio todos los campos del formulario
-      this.transactionForm.markAllAsTouched();
-      this.messageService.add({ severity: 'warn', summary: 'Error de Validacion', detail: 'Campos requeridos faltantes' });
-      return;
-    }
-
-    const formValues = this.transactionForm.value;
-    const isCreditCard: boolean = this.transactionForm.get('method')?.value === 'credito';
-    let mappedType: TransactionType;
-
-    if (formValues.type === 'income') mappedType = TransactionType.INCOME;
-    else if (formValues.type === 'expense') mappedType = TransactionType.EXPENSE;
-    else mappedType = TransactionType.PAYMENT;
-
-    //cambiamos esto y utilizamos la forma del payload por como manejamos los id y las subcuentas
-    //si usamos newTransaction: Transaction nuestro back espera un objeto completo de subAccount
-    //pero nosotros solo tenemos un string del id
-    //con payload as any creas un objeto libre. Lo usamos cuando lo que mostramos en pantalla es diferente a
-    //lo que guardamos
-    /*
-    const newTransaction: Transaction = {
-      amount: Number(formValues.amount),
-      type: mappedType,
-      subAccount: formValues.subAccountId,
-      isCreditCard: isCreditCard,
-      concept: formValues.concept,
-      transactionDate: formValues.date,
-      description: formValues.description
-    }; */
-
-    const payload = {
-      amount: Number(formValues.amount),
-      type: mappedType,
-      //pasamos directamente el id
-      subAccountId: formValues.subAccountId,
-      isCreditCard: isCreditCard,
-      concept: formValues.concept,
-      transactionDate: formValues.date,
-      description: formValues.description
-    };
-
-
-    if(this.transactionForm.valid) {
-      //usamos as any porque nuestro DTO del frontend no hace match al 100 con el del backend al momento de crear
-      this.transactionService.createTransaction(payload as any).subscribe({
-        next:(createdTx) => {
-          this.isTransactionVisible.set(false);
-          this.loadTransactions();
-          this.loadSubAccounts();
-          //recargamps para actualizar los saldos visuales
-          this.transactionForm.reset({
-              type: 'expense',
-              method: 'debito',
-              date: new Date()
-          });
-          this.messageService.add({ severity: 'success', summary: 'Transaccion Completada', detail: 'Status: verified' });
-        }, error: (err) => {
-          //BORRAR CONSOLE.LOG PARA PRODUCCION
-          console.error('Error al guardar en NestJS:', err);
-          this.messageService.add({ severity: 'error', summary: 'Fallo en la transaccion', detail: 'Desc: Autorizacion denegada' });
-        }
-      });
-    } else {
-      //BORRAR CONSOLE.LOG PARA PRODUCCION
-      console.warn("Formulario inválido, abortando envío.");
-      this.messageService.add({ severity: 'warn', summary: 'Error de Validacion', detail: 'Campos requeridos faltantes' });
-    }
-
-    //this.transactionForm.clearValidators()
-  }
 
   public deleteTransaction(id: string) {
     this.idDelete = id;
@@ -205,42 +120,6 @@ export class TransactionList implements OnInit {
         console.error('Error:', error);
       }
     })
-  }
-
-  private setupFormLogic() {
-    //escuchamos cada vez que cambia el tipo de transaccion
-    this.transactionForm.get('type')?.valueChanges.subscribe(selectedType => {
-      //si selecciona INGRESO o PAGO A TARJETA
-      if (selectedType === 'income' || selectedType === 'payment') {
-        //vamos a seleccionar forzosamente debito
-        this.transactionForm.get('method')?.setValue('debito');
-      }
-    });
-  }
-
-  public onMontoKeyPress(event: KeyboardEvent) {
-    if (event.key.length > 1 || event.ctrlKey || event.metaKey) {
-      return;
-    }
-    const allowedChars = /[0-9\.]/;
-    if (!allowedChars.test(event.key)) {
-      event.preventDefault();
-    }
-    if (event.key === '.') {
-      const currentValue = (event.target as HTMLInputElement).value;
-      if (currentValue.includes('.')) {
-        event.preventDefault();
-      }
-    }
-  }
-
-  public closeTransactionForm() {
-    this.isTransactionVisible.set(false)
-    this.transactionForm.reset({
-      type: 'expense',
-      method: 'debito',
-      date: new Date()
-    });
   }
 
   //esta linea era para que el HTML pudiera ver mi interfaz
--- nexus-web/src/app/components/transaction-modal/transaction-modal.ts
+++ nexus-web/src/app/components/transaction-modal/transaction-modal.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output} from '@angular/core';
 import {DatePicker} from "primeng/datepicker";
 import {Dialog} from "primeng/dialog";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Select} from "primeng/select";
+import {MessageService} from 'primeng/api';
+import {TransactionType} from '../../interfaces/transaction.interface';
+import {TransactionService} from '../../services/transaction';
+import {NgClass} from '@angular/common';
+import {SubAccount} from '../../interfaces/sub-account.interface';
 
 @Component({
   selector: 'app-transaction-modal',
@@ -10,11 +15,135 @@ import {Select} from "primeng/select";
       DatePicker,
       Dialog,
       ReactiveFormsModule,
-      Select
+      Select,
+    NgClass
   ],
   templateUrl: './transaction-modal.html',
   styleUrl: './transaction-modal.css',
 })
-export class TransactionModal {
+export class TransactionModal implements OnInit{
+  @Input() subAccountList: Array<SubAccount> = [];
+  @Input() isVisible: boolean = false;
+  @Output() isVisibleChange = new EventEmitter<boolean>();
+  @Output() transactionSaved = new EventEmitter<boolean>();
 
+
+  //usaremos los toasts del padre
+  private messageService = inject(MessageService);
+  private fb = inject(FormBuilder);
+  private transactionService = inject(TransactionService);
+
+  ngOnInit() {
+    this.setupFormLogic()
+  }
+
+  public transactionForm: FormGroup = this.fb.group({
+    amount: [null, [Validators.required, Validators.min(0.01)]],
+    type: ['expense', Validators.required], //valores: income, expense, payment
+    subAccountId: ['', Validators.required],
+    method: ['debito', Validators.required], //valores: debito, credito
+    concept: ['', Validators.required],
+    date: [new Date(), Validators.required],
+    description: ['']
+  });
+
+  //funcion que bloquea el btn de credito
+  private setupFormLogic() {
+    //escuchamos cada vez que cambia el tipo de transaccion
+    this.transactionForm.get('type')?.valueChanges.subscribe(selectedType => {
+      //si selecciona INGRESO o PAGO A TARJETA
+      if (selectedType === 'income' || selectedType === 'payment') {
+        //vamos a seleccionar forzosamente debito
+        this.transactionForm.get('method')?.setValue('debito');
+      }
+    });
+  }
+
+  //funcion para la logica de monto
+  public onMontoKeyPress(event: KeyboardEvent) {
+    if (event.key.length > 1 || event.ctrlKey || event.metaKey) {
+      return;
+    }
+    const allowedChars = /[0-9.]/;
+    if (!allowedChars.test(event.key)) {
+      event.preventDefault();
+    }
+    if (event.key === '.') {
+      const currentValue = (event.target as HTMLInputElement).value;
+      if (currentValue.includes('.')) {
+        event.preventDefault();
+      }
+    }
+  }
+
+  public closeTransactionForm() {
+    this.isVisibleChange.emit(false);
+    this.transactionForm.reset({
+      type: 'expense',
+      method: 'debito',
+      date: new Date()
+    });
+  }
+
+  public onSubmit() {
+    if (this.transactionForm.invalid) {
+      //finge que el usuario ya dio clic y ensucio todos los campos del formulario
+      this.transactionForm.markAllAsTouched();
+      this.messageService.add({ severity: 'warn', summary: 'Error de Validacion', detail: 'Campos requeridos faltantes' });
+      return;
+    }
+
+    const formValues = this.transactionForm.value;
+    const isCreditCard: boolean = this.transactionForm.get('method')?.value === 'credito';
+    let mappedType: TransactionType;
+
+    if (formValues.type === 'income') mappedType = TransactionType.INCOME;
+    else if (formValues.type === 'expense') mappedType = TransactionType.EXPENSE;
+    else mappedType = TransactionType.PAYMENT;
+
+    //cambiamos esto y utilizamos la forma del payload por como manejamos los id y las subcuentas
+    //si usamos newTransaction: Transaction nuestro back espera un objeto completo de subAccount
+    //pero nosotros solo tenemos un string del id
+    //con payload as any creas un objeto libre. Lo usamos cuando lo que mostramos en pantalla es diferente a
+    //lo que guardamos
+
+    const payload = {
+      amount: Number(formValues.amount),
+      type: mappedType,
+      //pasamos directamente el id
+      subAccountId: formValues.subAccountId,
+      isCreditCard: isCreditCard,
+      concept: formValues.concept,
+      transactionDate: formValues.date,
+      description: formValues.description
+    };
+
+
+    if(this.transactionForm.valid) {
+      //usamos as any porque nuestro DTO del frontend no hace match al 100 con el del backend al momento de crear
+      this.transactionService.createTransaction(payload as any).subscribe({
+        next:() => {
+          this.isVisibleChange.emit(false);
+          this.transactionSaved.emit(true);
+          //recargamps para actualizar los saldos visuales
+          this.transactionForm.reset({
+            type: 'expense',
+            method: 'debito',
+            date: new Date()
+          });
+          this.messageService.add({ severity: 'success', summary: 'Transaccion Completada', detail: 'Status: verified' });
+        }, error: (err) => {
+          //BORRAR CONSOLE.LOG PARA PRODUCCION
+          console.error('Error al guardar en NestJS:', err);
+          this.messageService.add({ severity: 'error', summary: 'Fallo en la transaccion', detail: 'Desc: Autorizacion denegada' });
+        }
+      });
+    } else {
+      //BORRAR CONSOLE.LOG PARA PRODUCCION
+      console.warn("Formulario inválido, abortando envío.");
+      this.messageService.add({ severity: 'warn', summary: 'Error de Validacion', detail: 'Campos requeridos faltantes' });
+    }
+
+    //this.transactionForm.clearValidators()
+  }
 }
