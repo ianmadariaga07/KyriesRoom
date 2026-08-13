435ce48732c1673c7d563b409e8283d4b4cb3ad2
ianmadariaga07 <iantranquis11@gmail.com>
2026-08-12 23:54:22 -0600

    feat(auth): implementacion del componente del login con validacion - Se genero el componente del login con sus respectivos archivos - le hicimos un refactor al auth-guard.ts, ahora esta en su propio directorio de guards

---
+++ nexus-web/src/app/components/login/login.ts
@@ -0,0 +1,40 @@
+import { Component, inject} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../services/auth';
+import {Router} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {MessageService} from 'primeng/api';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+
+export class Login {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private messageService = inject(MessageService);
+
+  loginForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.getRawValue()).subscribe({
+        next: () => {
+          this.messageService.add({ severity: 'success', summary: 'Inicio de Sesion Exitoso', detail: 'Status: verified' });
+          //redirigimos automáticamente a la vista de transacciones
+          this.router.navigate(['./transaccions']);
+        },
+        error: (err) => {
+          this.messageService.add({ severity: 'error', summary: 'Fallo en la operacion', detail: 'Desc: Autorizacion denegada'});
+        }
+      });
+    }
+  }
+}
